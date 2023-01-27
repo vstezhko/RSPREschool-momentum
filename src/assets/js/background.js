2c82f3a416c254f74d068d6618845aef3c6a9cde
vika <viktoriastezhko@gmail.com>
2023-01-27 16:59:21 +0300

    feat: add first random backround

--- src/assets/js/background.js
+++ src/assets/js/background.js
@@ -6,13 +6,15 @@ export const background = function (){
     const prevBtn = document.querySelector('.slide-prev')
 
     const images = ['01','02','03','04', '05', '06', '07', '08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
-    let index = 0
+    let index = Math.round(Math.random()*19)
 
     const body = document.querySelector('body')
 
     const setBackground = (node, images, index) => {
         node.style.backgroundImage = `url(../assets/img/${getPartOfDayGreeting()}/${images[index]}.webp)`
     }
+
+    setBackground(body, images, index)
 
     nextBtn.addEventListener('click', ()=>{
         console.log('next')
