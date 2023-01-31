62c4ee2c24e85a85cd3e4400ffac257e858ff195
vika <viktoriastezhko@gmail.com>
2023-01-31 22:45:57 +0300

    feat: add weather

---
+++ src/assets/js/weather.js
@@ -0,0 +1,69 @@
+export const weather = () => {
+
+    const cityInput = document.querySelector('input.city')
+    const weatherIcon = document.querySelector('.weather-icon')
+    const temperature = document.querySelector('.temperature')
+    const wind = document.querySelector('.wind')
+    const humidity = document.querySelector('.humidity')
+    const weatherDescription = document.querySelector('.weather-description')
+    const error = document.querySelector('.weather-error')
+
+    const onCityChange = () => {
+        if (city === cityInput.value || cityInput.value === '') {
+            cityInput.value = city
+            return
+        }
+        city = cityInput.value
+        localStorage.setItem('city', cityInput.value)
+        getWeather()
+    }
+    const onCityError = (data) => {
+        error.textContent = `Error: ${data.message} for city '${city}'`
+        weatherIcon.classList.remove(weatherIcon.classList[2])
+        temperature.textContent = ''
+        weatherDescription.textContent = ''
+        wind.textContent = ''
+        humidity.textContent = ''
+    }
+
+    cityInput.addEventListener('focus', ()=>{
+        cityInput.value = ''
+    })
+
+    cityInput.addEventListener('focusout', ()=>{
+        onCityChange()
+    })
+
+    cityInput.addEventListener('change', ()=>{
+        onCityChange()
+    })
+
+    let city = localStorage.getItem('city') || 'Минск'
+    const apiKey = '026c96d1416c803e3f5f3b9175665442'
+    const lang = 'ru'
+    const units = 'metric'
+
+
+    async function getWeather () {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=${lang}&appid=${apiKey}&units=${units}`
+        const resWeather = await fetch(url)
+        const data = await resWeather.json()
+        try {
+            data.weather[0].id
+        } catch {
+            onCityError(data)
+            return
+        }
+        error.textContent = ''
+        weatherIcon.classList.add(`owf-${data.weather[0].id}`);
+        temperature.textContent = `${Math.round(data.main.temp)}°C`;
+        weatherDescription.textContent = data.weather[0].description;
+        wind.textContent = `Wind speed: ${(data.wind.speed).toFixed(0)} m/s`
+        humidity.textContent = `Humidity: ${(data.main.humidity).toFixed(0)} %`
+    }
+
+    getWeather()
+
+    cityInput.value = city
+
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import {timeAndDateModule} from "./assets/js/timeAndDate"
 import {greetings} from "./assets/js/greetings";
 import  {background} from "./assets/js/background";
+import {weather} from "./assets/js/weather";
 
 
 window.onload = function() {
     timeAndDateModule()
     greetings()
     background()
+    weather()
 }
 
 
